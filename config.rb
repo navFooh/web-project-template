b11c63758c6fa217e5c2ac9f31ad86283dcbd8d3
Thijs van Hoof <thijs.v.h@gmail.com>
2014-11-05 19:23:37 +0100

    renamed images folder

--- config.rb
+++ config.rb
@@ -6,7 +6,7 @@ http_path = "./"
 css_dir = "./styles"
 sass_dir = "./styles"
 fonts_dir = "./assets/fonts"
-images_dir = "./assets/image"
+images_dir = "./assets/images"
 javascripts_dir = "./scripts"
 
 # Allow relative paths to assets via compass helper functions
