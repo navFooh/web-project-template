3b277abfcdd8a3538d00c9dea87330fc067d1e0b
Thijs van Hoof <thijs.v.h@gmail.com>
2015-03-20 10:18:07 +0900

    resize start and end

--- scripts/app/model/singleton/DisplayModel.js
+++ scripts/app/model/singleton/DisplayModel.js
@@ -1,36 +1,46 @@
 define(function () {
 
-	var DisplayModel = Backbone.Model.extend({
+	var debounce = 250,
+		DisplayModel = Backbone.Model.extend({
 
-		defaults: {
-			width: 0,
-			height: 0,
-			scrollTop: 0
-		},
+			defaults: {
+				width: 0,
+				height: 0,
+				scrollTop: 0
+			},
 
-		initialize: function () {
-			this.autoBind();
+			initialize: function () {
+				this.autoBind();
+				this.onScroll();
+				this.onResize();
 
-			this.onResize();
-			this.onScroll();
+				$(window).on('scroll', this.onScroll);
+				$(window).on('resize', this.onResize);
+				this.on('resize', this.onResizeStart);
+				this.on('resize', this.onResizeEnd);
+			},
 
-			$(window).on('resize', this.onResize);
-			$(window).on('scroll', this.onScroll);
-		},
+			onScroll: function () {
+				this.set({
+					scrollTop: $(window).scrollTop()
+				});
+			},
 
-		onResize: function () {
-			this.set({
-				width: window.innerWidth,
-				height: window.innerHeight
-			});
-		},
+			onResize: function () {
+				this.set({
+					width: window.innerWidth,
+					height: window.innerHeight
+				}).trigger('resize');
+			},
 
-		onScroll: function () {
-			this.set({
-				scrollTop: $(window).scrollTop()
-			});
-		}
-	});
+			onResizeStart: _.debounce(function() {
+				this.trigger('resizeStart');
+			}, debounce, true),
+
+			onResizeEnd: _.debounce(function() {
+				this.trigger('resizeEnd');
+			}, debounce)
+		});
 
 	return new DisplayModel();
 });
