f4a80dc3454163d8f435179d40cd3981836c5de9
Thijs van Hoof <thijs.v.h@gmail.com>
2016-08-24 15:30:23 +0200

    organize tasks

--- gulpfile.js
+++ gulpfile.js
@@ -11,26 +11,16 @@ require('gulp-require-tasks')({
 		pkg: JSON.parse(fs.readFileSync(__dirname + '/package.json')),
 		meta: JSON.parse(fs.readFileSync(__dirname + '/metadata.json')),
 
-		hbsStatic: {
-			src: __dirname + '/templates/static/index.hbs',
-			watch: __dirname + '/templates/static/**/*.hbs',
-			dest: __dirname + '/public',
-			name: 'index.html'
-		},
-
 		hbsRuntime: {
 			src: __dirname + '/templates/runtime/**/*.hbs',
 			dest: __dirname + '/templates/build'
 		},
 
-		sass: {
-			src: __dirname + '/styles/**/*.scss',
-			dest: __dirname + '/public/css',
-			options: { outputStyle: dev ? 'nested' : 'compressed' },
-			autoprefixer: {
-				browsers: ['last 2 versions'],
-				cascade: false
-			}
+		hbsStatic: {
+			src: __dirname + '/templates/static/index.hbs',
+			watch: __dirname + '/templates/static/**/*.hbs',
+			dest: __dirname + '/public',
+			name: 'index.html'
 		},
 
 		requirejs: {
@@ -46,11 +36,21 @@ require('gulp-require-tasks')({
 				generateSourceMaps: false,
 				optimize: 'uglify2'
 			}
+		},
+
+		sass: {
+			src: __dirname + '/styles/**/*.scss',
+			dest: __dirname + '/public/css',
+			options: { outputStyle: dev ? 'nested' : 'compressed' },
+			autoprefixer: {
+				browsers: ['last 2 versions'],
+				cascade: false
+			}
 		}
 	}]
 });
 
 gulp.task('default', dev
-	? ['hbs-static:watch', 'hbs-runtime:watch', 'requirejs:clean', 'sass:watch']
-	: ['hbs-static:compile', 'requirejs:compile', 'sass:compile']
+	? ['hbs-runtime:watch', 'hbs-static:watch', 'requirejs:clean', 'sass:watch']
+	: ['hbs-runtime:compile', 'hbs-static:compile', 'requirejs:compile', 'sass:compile']
 );
