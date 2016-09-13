a2a7d85da2e2d5e78c03888faec65eb42ebaf139
Thijs van Hoof <thijs.v.h@gmail.com>
2016-09-13 21:40:25 +0200

    remove footer

--- scripts/app/AppMain.js
+++ scripts/app/AppMain.js
@@ -1,16 +1,14 @@
 define([
 	'app/AppBase',
 	'view/Header',
-	'view/Content',
-	'view/Footer'
-], function (AppBase, Header, Content, Footer) {
+	'view/Content'
+], function (AppBase, Header, Content) {
 
 	return AppBase.extend({
 
 		createViews: function() {
 			new Header().render('body');
 			new Content().render('body');
-			new Footer().render('body');
 		}
 	});
 });
