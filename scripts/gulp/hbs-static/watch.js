5bba98808e2529ab63c165f950fa47e6b5d0f608
Thijs van Hoof <thijs.v.h@gmail.com>
2016-09-20 14:12:57 +0200

    execute hbs-runtime:compile on change

--- scripts/gulp/hbs-static/watch.js
+++ scripts/gulp/hbs-static/watch.js
@@ -1,18 +1,15 @@
-const compileHbs = require('gulp-compile-handlebars');
-const plumber = require('gulp-plumber');
-const rename = require('gulp-rename');
+const _ = require('underscore');
 const watch = require('gulp-watch');
 
 module.exports = {
 
-	dep: ['hbs-static:compile'],
+	fn: function(gulp, options) {
 
-	fn: function (gulp, options) {
-
-		return watch(options.hbsStatic.watch)
-			.pipe(plumber())
-			.pipe(compileHbs(options))
-			.pipe(rename(options.hbsStatic.name))
-			.pipe(gulp.dest(options.hbsStatic.dest));
+		return watch(options.hbsStatic.watch, {
+			ignoreInitial: false,
+			read: false
+		}, _.debounce(function() {
+			gulp.start('hbs-static:compile');
+		}, 100));
 	}
 };
