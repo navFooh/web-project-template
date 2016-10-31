28dea86427b484f14fc9ef656e38c5693040d8f9
Thijs van Hoof <thijs.v.h@gmail.com>
2016-10-31 21:08:37 +0100

    use native js for displaymodel

--- scripts/app/model/singleton/DisplayModel.js
+++ scripts/app/model/singleton/DisplayModel.js
@@ -1,41 +1,40 @@
 define(['backbone'], function (Backbone) {
 
-	var $window = $(window),
-		DisplayModel = Backbone.Model.extend({
+	var DisplayModel = Backbone.Model.extend({
 
-			defaults: {
-				width: 0,
-				height: 0,
-				scrollTop: 0
-			},
+		defaults: {
+			width: 0,
+			height: 0,
+			scrollX: 0,
+			scrollY: 0
+		},
 
-			initialize: function () {
-				_.bindAll(this, 'onResize', 'onScroll');
+		initialize: function () {
+			this.onResize();
+			this.onScroll();
 
-				this.onResize();
-				this.onScroll();
+			window.addEventListener('resize', this.onResize.bind(this));
+			window.addEventListener('scroll', this.onScroll.bind(this));
 
-				$window.on('resize', this.onResize);
-				$window.on('scroll', this.onScroll);
+			var debounce = 250;
+			this.on('resize', _.debounce(_.partial(this.trigger, 'resizeStart'), debounce, true));
+			this.on('resize', _.debounce(_.partial(this.trigger, 'resizeEnd'), debounce));
+		},
 
-				var debounce = 250;
-				this.on('resize', _.debounce(_.partial(this.trigger, 'resizeStart'), debounce, true));
-				this.on('resize', _.debounce(_.partial(this.trigger, 'resizeEnd'), debounce));
-			},
+		onResize: function () {
+			this.set({
+				width: window.innerWidth,
+				height: window.innerHeight
+			}).trigger('resize');
+		},
 
-			onResize: function () {
-				this.set({
-					width: window.innerWidth,
-					height: window.innerHeight
-				}).trigger('resize');
-			},
-
-			onScroll: function () {
-				this.set({
-					scrollTop: $window.scrollTop()
-				});
-			}
-		});
+		onScroll: function () {
+			this.set({
+				scrollX: window.scrollX || document.documentElement.scrollLeft,
+				scrollY: window.scrollY || document.documentElement.scrollTop
+			});
+		}
+	});
 
 	return new DisplayModel();
 });
