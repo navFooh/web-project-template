c00796a30a79f2a0961bb5b2fe1d8c09fa4f2376
Thijs van Hoof <thijs.v.h@gmail.com>
2017-06-02 10:31:33 +0200

    improve example view

--- scripts/app/model/example-model.js
+++ scripts/app/model/example-model.js
@@ -3,7 +3,8 @@ define(['backbone'], function (Backbone) {
 	return Backbone.Model.extend({
 
 		defaults: {
-			title: 'This is an example page'
+			title: 'This is an Example',
+			content: 'The example.js view renders the text from example-model.js into example.hbs'
 		}
 	});
 });
